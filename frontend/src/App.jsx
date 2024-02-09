18327bece33b49c6163e7d686cb6f619d859e019
tranguv <89886131+tranguv@users.noreply.github.com>
2024-02-09 14:49:55 -0500

    sua nav bar nhieu chut

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,29 @@
+import React from 'react';
 import Navbar from "./components/Navbar-stuffs/Navbar";
-import AuthForm from "./components/Main-Template/main-template"
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { navigation } from './router/navigation';
+import SideBarWrapper from './components/Main-Template/SideBarWrapper';
 
 function App() {
   return (
-    <div className="app-container">
-      <Navbar/>
-      <main className="main-content">
-        {/* Your main app content goes here */}
-      </main>
-    </div>
-  )
+    <Router>
+      <Routes>
+        {navigation.map((route, index) => {
+          const { path, component, isPrivate, noSideBar } = route;
+
+          const RouteComponent = () => noSideBar ? (
+            <>
+              {component}
+            </>
+          ) : (
+            <SideBarWrapper>{component}</SideBarWrapper>
+          );
+
+          return <Route key={index} path={path} element={<RouteComponent />} />;
+        })}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- frontend/src/components/Navbar-stuffs/Navbar.jsx
+++ frontend/src/components/Navbar-stuffs/Navbar.jsx
@@ -1,42 +1,86 @@
-import React, { useState } from 'react';
-import './Navbar.modules.css'; // Import your CSS file for styling
-import { Box } from '@chakra-ui/react';
-import { AiFillDatabase } from "react-icons/ai";
+import React from 'react';
+import {
+  FaTh,
+  FaBars,
+  FaUserAlt,
+  FaRegChartBar,
+  FaCommentAlt,
+  FaShoppingBag,
+  FaThList,
+} from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
+import './Navbar.modules.css';
 
-export default function Navbar() {
-  const [isSidebarCollapsed, setSidebarCollapsed] = useState(false);
-
-  const handleIconClick = () => {
-    setSidebarCollapsed(!isSidebarCollapsed);
-  };
-
-
+const Navbar = ({ children, isSidebarCollapsed, handleIconClick }) => {
+  const menuItem = [
+    {
+      path: '/',
+      name: 'Dashboard',
+      icon: <FaTh />,
+    },
+    {
+      path: '/about',
+      name: 'About',
+      icon: <FaUserAlt />,
+    },
+    {
+      path: '/analytics',
+      name: 'Analytics',
+      icon: <FaRegChartBar />,
+    },
+    {
+      path: '/comment',
+      name: 'Comment',
+      icon: <FaCommentAlt />,
+    },
+    {
+      path: '/product',
+      name: 'Product',
+      icon: <FaShoppingBag />,
+    },
+    {
+      path: '/productList',
+      name: 'Product List',
+      icon: <FaThList />,
+    },
+  ];
   return (
-    <nav className={`navbar ${isSidebarCollapsed ? 'collapsed' : ''}`}>
-      <Box className="icon">
-        <button onClick={handleIconClick}>
-          <a>
-            <AiFillDatabase size={20} />
-          </a>
-        </button>
-      </Box>
-
-      <div className="content">
-        <ul>
-          <li><a href="/">HOME</a></li>
-          <li><a href="/goal">Goal</a></li>
-          <li><a href="/report">Report</a></li>
-          <li><a href="/calendar">Calendar</a></li>
-          <li><a href="/logs">Logs</a></li>
-          <li><a href="/settings">Settings</a></li>
-          {/* Add more navigation items as needed */}
-        </ul>
+    <div className='container'>
+      <div style={{ width: 'fit-content' }} className='sidebar'>
+        <div className='top_section'>
+          <h1
+            style={{ display: isSidebarCollapsed ? 'block' : 'none' }}
+            className='logo'
+          >
+            Logo
+          </h1>
+          <div
+            style={{ marginLeft: isSidebarCollapsed ? '50px' : '0px' }}
+            className='bars'
+          >
+            <FaBars onClick={handleIconClick} />
+          </div>
+        </div>
+        {menuItem.map((item, index) => (
+          <NavLink
+            to={item.path}
+            key={index}
+            className='link'
+            activeclassName='active'
+          >
+            <div className='icon'>{item.icon}</div>
+            <div
+              style={{ display: isSidebarCollapsed ? 'block' : 'none' }}
+              className='link_text'
+            >
+              {item.name}
+            </div>
+          </NavLink>
+        ))}
       </div>
-
-      <Box className={`logout-button ${isSidebarCollapsed ? 'hidden' : ''}`} mt="auto">
-      <button><a>Logout</a></button>
-      </Box>
-
-    </nav>
+      <main>{children}</main>
+    </div>
   );
-}
+};
+
+export default Navbar;
