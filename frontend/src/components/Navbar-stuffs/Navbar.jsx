19b743296c9f9b4986571bc39d6ba3e10ce75b4e
Quang Tran <deathscout2003@gmail.com>
2024-02-08 20:29:18 -0500

    Finish navbar, with the collapsible feature

--- frontend/src/components/Navbar-stuffs/Navbar.jsx
+++ frontend/src/components/Navbar-stuffs/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './.modules.css'; // Import your CSS file for styling
+import './Navbar.modules.css'; // Import your CSS file for styling
 import { Box } from '@chakra-ui/react';
 import { AiFillDatabase } from "react-icons/ai";
 
@@ -10,51 +10,33 @@ export default function Navbar() {
     setSidebarCollapsed(!isSidebarCollapsed);
   };
 
+
   return (
-    
+    <nav className={`navbar ${isSidebarCollapsed ? 'collapsed' : ''}`}>
+      <Box className="icon">
+        <button onClick={handleIconClick}>
+          <a>
+            <AiFillDatabase size={20} />
+          </a>
+        </button>
+      </Box>
 
-    <nav className={`navbar ${isSidebarCollapsed ? 'collapsed' : ''}`} style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
-      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '1px' }}>
-        <div>
-          <Box className="icon">
-            <button onClick={handleIconClick}>
-              <a href="/icon">
-                <AiFillDatabase size={20} />
-              </a>
-            </button>
-          </Box>
-        </div>
-      </div>
-
-      <div className="content" style={{ flex: 1, display: 'flex', alignItems: 'center' }}>
+      <div className="content">
         <ul>
-          <li>
-            <a href="/">HOME</a>
-          </li>
-          <li>
-            <a href="/goal">Goal</a>
-          </li>
-          <li>
-            <a href="/report">Report</a>
-          </li>
-          <li>
-            <a href="/calendar">Calendar</a>
-          </li>
-          <li>
-            <a href="/logs">Logs</a>
-          </li>
-          <li>
-            <a href="/settings">Settings</a>
-          </li>
+          <li><a href="/">HOME</a></li>
+          <li><a href="/goal">Goal</a></li>
+          <li><a href="/report">Report</a></li>
+          <li><a href="/calendar">Calendar</a></li>
+          <li><a href="/logs">Logs</a></li>
+          <li><a href="/settings">Settings</a></li>
           {/* Add more navigation items as needed */}
         </ul>
       </div>
 
-      <Box className="logout-button" mt="auto">
-        <button><a href="/logout">Logout</a></button>
+      <Box className={`logout-button ${isSidebarCollapsed ? 'hidden' : ''}`} mt="auto">
+      <button><a>Logout</a></button>
       </Box>
+
     </nav>
   );
 }
-
-
