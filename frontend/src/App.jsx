956c30d80bfe5406fa11c96d97d09ac1df261725
minhanhhh03 <99114358+minhanhhh03@users.noreply.github.com>
2024-02-12 00:12:17 -0500

    setting ui page 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { navigation } from './router/navigation';
-
+import SignIn from './components/sign_in/sign_in';
+import SignUp from './components/sign_up/sign_up';
 // css
 import './App.css';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        {navigation.map((route, index) => {
-          const { path, component: Component, isPrivate, noLayoutWrap } = route;
-
-          const element = Component;
-          // noLayoutWrap ? (  // cmt tam khi nao co side bar bo vo
-          //   <Component />
-          // ) : (
-          //   <LayoutWrapper>
-          //     <Component />
-          //   </LayoutWrapper>
-          // );
-
-          return <Route key={index} path={path} element={element} />;
-        })}
-      </Routes>
-    </Router>
-  );
 }
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
 import { ChakraProvider } from "@chakra-ui/react"
-
+import SignUp from './components/sign_up/sign_up.jsx'
+import AuthForm from './components/Main-Template/main-template.jsx'
+import SignIn from './components/sign_in/sign_in.jsx'
+import Settingpg from './components/Settings/settings.jsx'
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <Settingpg></Settingpg>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
