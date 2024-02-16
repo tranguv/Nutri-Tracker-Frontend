40bcc3266a84b0424c76c2c906453b58bbbe7c65
chiemgiabaost <bao2003@my.yorku.ca>
2024-02-15 21:31:41 -0500

    fixing layout for dasboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,17 +1,57 @@
 import Process from "../components/process-component/Process";
+import Achievement from "../components/process-component/Achievement";
 const Dasboard = () => {
   return (
-    <div className="flex flex-col justify-center items-center "> {/* Apply flexbox properties to center vertically */}
-      <div className="flex justify-center items-center w-2/3 h-32 bg-blue-200 m-2 rounded-lg">
-        {/* Content inside this div */}
+    <div className="flex flex-col justify-center items-center gap-8 mt-10 mb-10 mx-16 "> {/* Added gap-8 for consistent vertical spacing */}
+      <div className="flex w-full  h-32 bg-blue-200 rounded-lg items-center justify-center"> {/* Center content vertically and horizontally */}
+        <p className="p-2 text-gray-500">
+          Hi, ...
+        </p>
       </div>
-      <div className="flex justify-center flex-wrap gap-4">
-        <Process valuePercent="90" />
-        <Process valuePercent="65" icon='rice' />
-        <Process valuePercent="45" icon="water" />
+
+      <div className="flex w-full gap-10"> {/* Added gap-10 for horizontal gap */}
+        <div className="w-2/3">
+          <div className="flex flex-col justify-center mt-8 gap-6"> {/* Added gap-6 for vertical gap */}
+            <div className="flex gap-10">
+              <div className="w-1/2">
+                <Process valuePercent="90" activity="Steps" />
+              </div>
+              <div className="w-1/2">
+                <Process valuePercent="100" icon='rice' activity="Cal" />
+              </div>
+            </div>
+            <div className="h-36 bg-gray-300 rounded-lg"></div>
+            <div className="flex gap-10">
+              <div className="w-1/2">
+                  <Achievement />
+              </div>
+              <div className="w-1/2">
+                <Process valuePercent="100" icon='rice' activity="Cal" height="36"/>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col w-1/3 mt-8 gap-6"> {/* Added gap-6 for vertical gap */}
+          <Process valuePercent="90" activity="Steps" />
+          <div className="h-36 bg-gray-300 rounded-lg"></div>
+          <div className="flex gap-10">
+              <div className="w-full">
+                <Process valuePercent="90" activity="Steps" height="36"/>
+              </div>
+              
+            </div>
+        </div>
+      </div>
+
+      <div className="w-full h-32 bg-red-300 rounded-lg flex gap-2"></div>
+
+      <div className="flex justify-center flex-wrap gap-10 w-2/3">
+        
       </div>
     </div>
   );
 };
+
 
 export default Dasboard;
