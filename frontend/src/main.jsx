8076bc1b37551dc1008e7f9b4a9917a75a1f035b
minhanhhh03 <99114358+minhanhhh03@users.noreply.github.com>
2024-02-15 21:17:37 -0500

    t rut gon lai code

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,11 @@ import App from './App.jsx'
 import { createRoot } from 'react-dom/client'
 import { ChakraProvider } from "@chakra-ui/react"
 import './index.css'
+import Settingpg from './components/Settings/settings.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <Settingpg></Settingpg>
   </React.StrictMode>,
   document.getElementById('root')
 )
