12ea8d54a2f34bb6ac2a09be993906b698d92bb5
Quang Tran <deathscout2003@gmail.com>
2024-03-04 18:32:21 -0500

    Badge/achivement display
    The feature of traversing the badges within the container is done. Unable to utilize react icons to make the Ui more appealing.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
-import Navbar from "./components/Navbar-stuffs/Navbar";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { navigation } from './router/navigation';
-import SideBarWrapper from './components/Main-Template/SideBarWrapper';
+// import Navbar from "./components/Navbar-stuffs/Navbar";
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { navigation } from './router/navigation';
+// import SideBarWrapper from './components/Main-Template/SideBarWrapper';
+import Badge from "./components/badge/Badge";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {navigation.map((route, index) => {
-          const { path, component, isPrivate, noSideBar } = route;
+    // <Router>
+    //   <Routes>
+    //     {navigation.map((route, index) => {
+    //       const { path, component, isPrivate, noSideBar } = route;
 
-          const RouteComponent = () => noSideBar ? (
-            <>
-              {component}
-            </>
-          ) : (
-            <SideBarWrapper>{component}</SideBarWrapper>
-          );
+    //       const RouteComponent = () => noSideBar ? (
+    //         <>
+    //           {component}
+    //         </>
+    //       ) : (
+    //         <SideBarWrapper>{component}</SideBarWrapper>
+    //       );
 
-          return <Route key={index} path={path} element={<RouteComponent />} />;
-        })}
-      </Routes>
-    </Router>
+    //       return <Route key={index} path={path} element={<RouteComponent />} />;
+    //     })}
+    //   </Routes>
+    // </Router>
+    <div> <Badge/> </div>
   );
 }
 
--- frontend/src/components/Navbar-stuffs/Navbar.jsx
+++ frontend/src/components/Navbar-stuffs/Navbar.jsx
@@ -1,107 +1,109 @@
-import React from 'react';
-import {
-  FaTh,
-  FaBars,
-  FaUserAlt,
-  FaRegChartBar,
-  FaCommentAlt,
-  FaShoppingBag,
-  FaThList,
-  FaSignOutAlt,
-  FaDelicious,
-  FaRunning,
-  FaGamepad,
-  FaHome,
-} from 'react-icons/fa';
-import { NavLink } from 'react-router-dom';
-import './Navbar.modules.css';
-import { color } from 'framer-motion';
+// import React from 'react';
+// import {
+//   FaTh,
+//   FaBars,
+//   FaUserAlt,
+//   FaRegChartBar,
+//   FaCommentAlt,
+//   FaShoppingBag,
+//   FaThList,
+//   FaSignOutAlt,
+//   FaDelicious,
+//   FaRunning,
+//   FaGamepad,
+//   FaHome,
+// } from 'react-icons';
+// import { NavLink } from 'react-router-dom';
+// import './Navbar.modules.css';
+// import { color } from 'framer-motion';
 
-const Navbar = ({ children, isSidebarCollapsed, handleIconClick }) => {
-  const menuItem = [
-    {
-      path: '/dashboard',
-      name: 'Dashboard',
-      icon: <FaHome />,
-    },
-    {
-      path: '/reports',
-      name: 'Reports',
-      icon: <FaRegChartBar />,
-    },
-    {
-      path: '/logs',
-      name: 'Logs',
-      icon: <FaRunning />,
-    },
-    {
-      path: '/coinstore',
-      name: 'Coin Store',
-      icon: <FaGamepad />,
-    },
-    {
-      path: '/profile',
-      name: 'Profile',
-      icon: <FaUserAlt />,
-    },
-    {
-      path: '/logout',
-      name: 'Log Out',
-      icon: <FaSignOutAlt />,
-    },
-  ];
-  return (
-    <div className='container'>
-      <div
-        style={{
-          width: 'fit-content',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-        className='sidebar'
-      >
-        <div className='top_section'>
-          <h1
-            style={{ display: isSidebarCollapsed ? 'block' : 'none' }}
-            className='logo'
-          >
-            Logo
-          </h1>
-          <div
-            style={{ marginLeft: isSidebarCollapsed ? '50px' : '0px' }}
-            className='bars'
-          >
-            <FaBars onClick={handleIconClick} />
-          </div>
-        </div>
-        <div>
-          {menuItem.map((item, index) => (
-            <NavLink
-              to={item.path}
-              key={index}
-              className='link'
-              activeclassName='active'
-            >
-              <div className='icon' style={{ color: 'black' }}>
-                {item.icon}
-              </div>
-              <div
-                style={{
-                  display: isSidebarCollapsed ? 'block' : 'none',
-                  color: 'black',
-                }}
-                className='link_text'
-              >
-                {item.name}
-              </div>
-            </NavLink>
-          ))}
-        </div>
-      </div>
-      <main>{children}</main>
-    </div>
-  );
-};
+
+
+// const Navbar = ({ children, isSidebarCollapsed, handleIconClick }) => {
+//   const menuItem = [
+//     {
+//       path: '/dashboard',
+//       name: 'Dashboard',
+//       icon: <FaHome />,
+//     },
+//     {
+//       path: '/reports',
+//       name: 'Reports',
+//       icon: <FaRegChartBar />,
+//     },
+//     {
+//       path: '/logs',
+//       name: 'Logs',
+//       icon: <FaRunning />,
+//     },
+//     {
+//       path: '/coinstore',
+//       name: 'Coin Store',
+//       icon: <FaGamepad />,
+//     },
+//     {
+//       path: '/profile',
+//       name: 'Profile',
+//       icon: <FaUserAlt />,
+//     },
+//     {
+//       path: '/logout',
+//       name: 'Log Out',
+//       icon: <FaSignOutAlt />,
+//     },
+//   ];
+//   return (
+//     <div className='container'>
+//       <div
+//         style={{
+//           width: 'fit-content',
+//           display: 'flex',
+//           flexDirection: 'column',
+//           alignItems: 'center',
+//         }}
+//         className='sidebar'
+//       >
+//         <div className='top_section'>
+//           <h1
+//             style={{ display: isSidebarCollapsed ? 'block' : 'none' }}
+//             className='logo'
+//           >
+//             Logo
+//           </h1>
+//           <div
+//             style={{ marginLeft: isSidebarCollapsed ? '50px' : '0px' }}
+//             className='bars'
+//           >
+//             <FaBars onClick={handleIconClick} />
+//           </div>
+//         </div>
+//         <div>
+//           {menuItem.map((item, index) => (
+//             <NavLink
+//               to={item.path}
+//               key={index}
+//               className='link'
+//               activeclassName='active'
+//             >
+//               <div className='icon' style={{ color: 'black' }}>
+//                 {item.icon}
+//               </div>
+//               <div
+//                 style={{
+//                   display: isSidebarCollapsed ? 'block' : 'none',
+//                   color: 'black',
+//                 }}
+//                 className='link_text'
+//               >
+//                 {item.name}
+//               </div>
+//             </NavLink>
+//           ))}
+//         </div>
+//       </div>
+//       <main>{children}</main>
+//     </div>
+//   );
+// };
 
 export default Navbar;
