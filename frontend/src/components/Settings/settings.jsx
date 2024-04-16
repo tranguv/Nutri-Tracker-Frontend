86ec1d862963f32ec877220fdfec09db5f51de41
minhanhhh03 <99114358+minhanhhh03@users.noreply.github.com>
2024-04-16 10:46:03 -0400

    Minh Anh sửa lại phần profile setting

--- frontend/src/components/Settings/settings.jsx
+++ frontend/src/components/Settings/settings.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./settings.module.css"; // Import as a module
+import avatarImage from "./avatar.png";
 
 const Settingpg = () => {
   const Edit = () => {
@@ -8,26 +9,54 @@ const Settingpg = () => {
   };
 
   const placeholders = [
-    { label: "Full Name" },
-    { label: "Date of Birth" },
-    { label: "Gender" },
-    { label: "Blood Type" },
-    { label: "Email Address" },
-    { label: "Unit of measurement" },
+    { label: "Gia Bao Chiem" },
+    { label: "04/02/2000" },
+    { label: "Female" },
+    { label: "Type AB" },
+    { label: "abc@gmail.com" },
+    { label: "168cm" },
+    { label: "50kg" },
   ];
 
   return (
-    <div className={styles.placeholderContainer}>
-      {/* Mapping through the array to render placeholders */}
-      {placeholders.map((placeholder, index) => (
-        <div key={index} className={styles.placeholder}>
-          <div className={styles.placeholderBackground} />
-          <div className={styles.placeholderText}>{placeholder.label}</div>
+    <div className="flex flex-col items-center h-screen relative">
+      {/* Flex container for the avatar and all placeholders */}
+      <div className="flex items-start">
+        {/* Avatar on the left */}
+        <div className="w-10 h-12 bg-violet-300 rounded-2xl overflow-hidden">
+          <img
+            src={avatarImage}
+            className="w-full h-full object-cover"
+            alt="Avatar"
+          />
         </div>
-      ))}
-      {/* Edit button */}
-      <div className={styles.EditButtonContainer}>
-        <button onClick={Edit}>Edit</button>
+
+        {/* Placeholders including 'Your Profile' on the right */}
+        <div className="ml-4">
+          {/* 'Your Profile' text right on top of the full name */}
+          <div className="text-black font-semibold font-['Nunito Sans'] mb-2">
+            Your Profile
+          </div>
+          <div className="flex flex-col">
+            {placeholders.map((placeholder, index) => (
+              <div
+                key={index}
+                className="w-96 py-4 bg-gray-300 rounded-lg flex justify-center items-center my-2"
+              >
+                <div className="text-black font-HermeneusOne">
+                  {placeholder.label}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      {/* Edit button at the bottom right */}
+      <div className="absolute bottom-0 right-0 mr-20 mb-10 mt-5">
+        <button onClick={Edit} className="bg-violet-300 text-black font-medium px-8 py-2 rounded-lg">
+          Edit
+        </button>
       </div>
     </div>
   );
