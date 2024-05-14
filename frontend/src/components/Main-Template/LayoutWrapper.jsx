35f9576307eaa60cd4f77700a96bc752e396ea87
tranguv <89886131+tranguv@users.noreply.github.com>
2024-05-13 20:48:52 -0400

    change nav bar color

--- frontend/src/components/Main-Template/LayoutWrapper.jsx
+++ frontend/src/components/Main-Template/LayoutWrapper.jsx
@@ -6,36 +6,6 @@ import { useStateContext } from '../../contexts/ContextProvider';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import { FiSettings } from 'react-icons/fi';
 
-// const LayoutWrapper = ({ children }) => {
-//   const { isSidebarCollapsed, setSidebarCollapsed } = useStateContext();
-
-//   const handleIconClick = () => {
-//     setSidebarCollapsed(!isSidebarCollapsed);
-//   };
-
-//   // const sidebarWidth = isSidebarCollapsed ? '170px' : '50px';
-
-//   return (
-//     <div style={{ display: 'flex' }}>
-//       <Sidebar
-//         isSidebarCollapsed={isSidebarCollapsed}
-//         handleIconClick={handleIconClick}
-//       />
-//       <div style={{ height: '100vh', width: '100%' }}>
-//         <Navbar />
-//         <div>{children}</div>
-//       </div>
-//       <TooltipComponent
-//         content='Settings Tooltip'
-//         style={{ position: 'fixed', bottom: '20px', right: '20px' }}
-//       >
-//         <FiSettings size={30} />
-//       </TooltipComponent>
-//     </div>
-//   );
-// };
-
-// export default LayoutWrapper;
 const LayoutWrapper = ({ children }) => {
   const { isSidebarCollapsed, setSidebarCollapsed } = useStateContext();
 
@@ -54,11 +24,8 @@ const LayoutWrapper = ({ children }) => {
       <div
         style={{
           height: '100vh',
-          // width: 'calc(100% - 10rem)',
           width: '100%',
           marginLeft: sidebarWidth,
-          // marginRight: 'calc(100% - 2rem)'
-          // overflowX: 'hidden',
         }}
       >
         <div>
@@ -67,10 +34,7 @@ const LayoutWrapper = ({ children }) => {
             style={{
               position: 'relative',
               top: '80px',
-              // overflowY: 'scroll',
-              // overflowX: 'hidden',
               height: 'calc(100vh - 82px)',
-              // width: 'calc(100% - 14rem)',
             }}
           >
             {children}
